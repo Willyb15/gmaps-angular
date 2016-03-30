300bffde52e473bb3d7aece805ff6f21c61ad0d2
Willyb15 <william.steen.bryant@gmail.com>
2016-03-30 16:18:26 -0400

    updated google maps app
    added info buttons, and a directions function

--- controller.js
+++ controller.js
@@ -1,55 +1,92 @@
 var mapsApp = angular.module('mapsApp', []);
 mapsApp.controller('mapsController', function ($scope){
-
-       $scope.map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          //geographical center of the US
-          center: new google.maps.LatLng(40.0000, -98.0000)
-        });
-
-
-	function createMarker (city){
+  $scope.markers = [];
+  $scope.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    //geographical center of the US
+    center: new google.maps.LatLng(40.0000, -98.0000)
+  });
+  
+  infowindow = new google.maps.InfoWindow;
+	
+  function createMarker (city){
 		var latLon = city.latLon.split(',');
 		var lat = latLon[0];
 		var lon = latLon[1];
-		var marker = new google.maps.Marker({
+		// passing object into some Google Maps predefined functions
+    var marker = new google.maps.Marker(
+    {
 			map: $scope.map,
 			position: new google.maps.LatLng(lat, lon),
-			title: city.city
-		});
+			title: city.city,
+      animation: google.maps.Animation.DROP 
+    });
+    // Create new html for "contentString", pulled from cities object, put into th InfoWindow
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '<h2>' + city.city + ", " + city.state + '</h2>'+
+      '<div class="total-pop">Total Population: ' + city.yearEstimate + '</div>' +
+      '<div class="pop-dens-last-year">2010 Census: ' + city.lastCensus + '</div>' +
+      '<div class="yearRank">US Population Rank: ' + city.yearRank + '</div>' +
+      '<div class="pop-change">Population Change %: ' + city.change + '</div>' +
+      '<div class="land-area">Land Area: ' + city.landArea + '</div>' +
+      '<div class="land-areaInKM">Land Area Imperial: ' + city.landAreaInKm + '</div>' +
+      '<div class="pop-dens">Population Density: ' + city.lastPopDensity + '</div>' +
+      '<div class="directions"><a href="" onclick=getDirections('+lat+','+lon+');>Get Directions!</a></div>' +
+      '</div>';
 
-        var contentString = '<div id="content">'+
-          '<div id="siteNotice">'+
-          '<h1>' + city.city + ", " + city.state + '</h1>' +
-          '<div class="yearRank">US Population Rank: ' + city.yearRank + '</div>' +
-          '<div class="total-pop">Total Population: ' + city.yearEstimate + '</div>' +
-          '<div class="pop-dens-last-year">2010 Census: ' + city.lastCensus + '</div>' +
-          '<div class="pop-change">Population Change %: ' + city.change + '</div>' +
-           '<div class="land-area">Land Area: ' + city.landArea + '</div>' +
-          '<div class="land-areaInKM">Land Area Imperial: ' + city.landAreaInKm + '</div>' +
-          '<div class="pop-dens">Population Density: ' + city.lastPopDensity + '</div>' +
-           '<div class="pop-dens">Population Density Imperial: ' + city.lastPopDensityInKM + '</div>' +
-          '<div class="state">State: ' + city.state + '</div>' +
-          '<div class="Lat and Long">' + "Lat="+ lat + ',' +"Lon=" + lon + '</div>' +
-          '</div>';
+    marker.addListener('click', function(){
+      infowindow.setContent(contentString)
+      infowindow.open($scope.map, marker);
+    });
 
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
+  
+    
+    // Push markers into empty array($scope.markers[];) defined above
+    $scope.markers.push(marker);
+  }
 
-        marker.addListener('click', function() {
-          infowindow.open($scope.map, marker);
-        });
+  $scope.zoomTo = function(i){
+      var latLon = cities[i].latLon.split(',');
+      var newLat = Number(latLon[0]);
+      var newLon = Number(latLon[1]);
+  
+    $scope.map.setCenter({
+      lat : newLat,
+      lng : newLon
+    });
 
-	}
+    $scope.map.setZoom(9);
+  }
 
-  $scope.triggerClick = function(i){
-    google.maps.event.trigger($scope.markers[i-1],'click');
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap($scope.map);
+
+  getDirections = function(lat, lon){   
+       var request = {
+         origin: 'Atlanta, GA', 
+         destination:new google.maps.LatLng(lat,lon), 
+         travelMode: google.maps.DirectionsTravelMode.DRIVING
+       };
+       directionsService.route(request, function(response, status) {
+         if (status == google.maps.DirectionsStatus.OK) {
+           directionsDisplay.setDirections(response);
+         }
+       }); 
+      event.preventDefault();
+  }
+  
+
+  
+
+  // when you click on cityClick find the i-th element in markers
+  $scope.cityClick = function(i){
+    google.maps.event.trigger($scope.markers[i],'click');
   }
 
 	$scope.cities = cities;
 	for(i = 0; i< cities.length; i++){
 		createMarker(cities[i])
 	}
-
 });
